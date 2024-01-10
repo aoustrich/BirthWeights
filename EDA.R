d8238ec7cafc8331bc59da13b4c7e1e02651570a
Aaron Oustrich <aaron.oustrich@gmail.com>
2024-01-09 18:52:05 -0700

    Setup EDA

---
+++ EDA.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+
+# Read Data ---------------------------------------------------------------
+
+weights <- read_table("data/BirthWeights.txt")
+
+# Convert Race and Gen cols to factors
+weights <- weights %>% 
+  mutate_at(vars(Race,Gen),factor)
+
+
+# EDA ---------------------------------------------------------------------
